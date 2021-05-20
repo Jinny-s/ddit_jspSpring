bbb537543ce7d61d7b52a55b50c77ed37429507f
Jinny-s <46083003+Jinny-s@users.noreply.github.com>
2021-05-20 15:36:02 +0900

    bootstrap mybatis commit

---
+++ command_bootstrap/src/kr/or/ddit/service/MenuServiceImpl.java
@@ -0,0 +1,27 @@
+package kr.or.ddit.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.or.ddit.dto.MenuVO;
+
+public class MenuServiceImpl implements MenuService {
+
+	
+	
+	@Override
+	public List<MenuVO> getMainMenuList() throws SQLException {
+		return null;
+	}
+
+	@Override
+	public List<MenuVO> getSubMenuList(String mCode) throws SQLException {
+		return null;
+	}
+
+	@Override
+	public MenuVO getMenuByMcode(String mCode) throws SQLException {
+		return null;
+	}
+
+}
